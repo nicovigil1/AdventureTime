414c36fb875fbdcfe5b8dca8b62f098bf2d4b8c1
Nico <nicovigil1@gmail.com>
2019-04-10 16:37:41 -0600

    Adds arena / canvas for battles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './components/Header'
 import Login from './components/Login'
 import Welcome from './components/Welcome'
-import Canvas from './components/Canvas'
+import Arena from './components/Arena'
 class App extends Component {
   constructor() {
     super()
@@ -12,19 +12,59 @@ class App extends Component {
       loginInfo: [],
       loginDisplay: true,
       currentUser: null,
+      currentQuest: null,
+      fight: false,
+      enemyHealth: 10
     }
   }
 
   toggleLoginDisplay = () => {
     this.setState({loginDisplay: !this.state.loginDisplay})
   }
-
+  
   getCurrentUser = (user) => {
     this.setState({currentUser: user})
   }
+  
+  setTheState = (k_v) => {
+    this.setState(k_v)  
+  }
+  
+  attackEnemy = () => {
+    let randomizer =  Math.floor(Math.random() * 10)
+    if (this.state.enemyHealth > 0) {
+      this.setState({enemyHealth: this.state.enemyHealth-2})
+      if (randomizer < 4) {
+        this.setState({playerHealth: this.state.playerHealth-2})
+      }
+    } 
+    this.checkFightStatus()
+  }
 
-  getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition(location => { console.log(location) }) //(location => {return location})
+  checkFightStatus = () => {
+    if (this.state.enemyHealth <= 2) {
+      fetch(`https://adventure-time-m4cap.herokuapp.com/api/v1/encounter?success=true&user_id=${this.state.currentUser.ID}`, {method: "POST"})
+      .then(res => res.json())
+      .then(res => this.setState({questComplete: res.quest_complete}))
+      .then(res => {
+        this.setState({ enemyHealth: 10 })
+        this.setState({ fight: false })
+        this.checkQuestCompletion()
+      })
+    } else if (this.state.playerHealth <= 2) {
+      fetch(`https://adventure-time-m4cap.herokuapp.com/api/v1/encounter?success=false&user_id=${this.state.currentUser.ID}`, { method: "POST" })
+      .then(res => res.json())
+      .then(this.setState({fight: false}))
+      alert("you do not know de wey")
+    }
+  }
+
+  checkQuestCompletion = () => {
+    if (this.state.questComplete) {
+      alert("congrats, you won the quest!")
+    } else {
+      alert("you won the battle, but not the war")
+    }
   }
 
   render() {
@@ -32,13 +72,23 @@ class App extends Component {
       <div style={styleBody}>
         < Header />
         <div style={styleLogin}>
-          {this.state.loginDisplay && <Login display={this.toggleLoginDisplay} 
-                                             getLocation={this.getCurrentLocation}
+          {!this.state.fight && this.state.loginDisplay && <Login display={this.toggleLoginDisplay} 
                                              getUser={this.getCurrentUser}/>}
-          {!this.state.loginDisplay && <Welcome currentUser={this.state.currentUser} />}
-        </div>
-        <div>
-          < Canvas />
+          {!this.state.fight && !this.state.loginDisplay && <Welcome currentUser={this.state.currentUser} 
+                                                location={this.state.currentLocation}
+                                                setTheState={this.setTheState}
+                                        />}
+          {this.state.fight && <div style={styleHealth}>
+            <button onClick={this.attackEnemy}>Attack</button>
+            <p>
+              Your Health: {this.state.playerHealth}
+            </p>
+            <p>
+              Enemy Health: {this.state.enemyHealth}
+            </p>
+          </div>}
+          {this.state.fight && < Arena />}
+          {/* {this.state.fight && <button>Attack</button>} */}
         </div>
       </div>
     );
@@ -54,8 +104,17 @@ const styleLogin = {
 
 const styleBody = {
   height: "100vh",
-  backgroundImage: "url('https://i.pinimg.com/736x/3c/1e/d3/3c1ed3034e801dcb19e06558501fd457.jpg')",
-  backgroundSize: "cover"
+  backgroundImage: "url('https://i.pinimg.com/originals/f3/64/b7/f364b7d0a49566891789d291b8b2e53f.png')",
+  backgroundSize: "cover",
+  imageRendering: "pixelated"
 }
 
+const styleHealth = {
+  position: "absolute",
+  marginLeft: "auto",
+  marginRight: "auto",
+  background: "rgba(206, 215, 219, 0.96)",
+  padding: "3em",
+  borderRadius: "1em"
+}
 export default App;
---
+++ src/components/Arena.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Spritesheet from 'react-responsive-spritesheet';
+
+export default class Arena extends React.Component {
+  render() {
+    return(
+      <div style={styleFlex}>
+          <Spritesheet
+            id="player"
+            style={stylePlayer}
+            image={`https://i.ibb.co/Kjd23z4/adventurer-1-3-Sheet.png`}
+            widthFrame={50}
+            heightFrame={37}
+            steps={4}
+            fps={6}
+            autoplay={true}
+            loop={true} />
+          <Spritesheet
+            id="enemy"
+            style={styleEnemy}
+            image={`https://i.ibb.co/jyBGFQJ/Heavy-Bandit-Spritesheet.png`}
+            widthFrame={48}
+            heightFrame={48}
+            steps={4}
+            fps={6}
+            autoplay={true}
+            loop={true} />
+      </div>
+    )
+  }
+}
+
+const styleEnemy = {  
+  height: "200px", 
+  width: "200px",
+  position: "relative",
+  // top: "20vh",
+  right: "13vw"
+}
+
+const stylePlayer = {  
+  height: "200px", 
+  width: "200px",
+  alignSelf: "flex-end",
+  position: "relative",
+  // top: "20vh"
+  // left: "12.5vw",
+}
+
+const styleFlex = {
+  display: "flex",
+  flexDirection: "row",
+  alignSelf: "flex-end",
+  width: "100vw"
+}
