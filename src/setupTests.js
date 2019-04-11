a054986f58764d9c6b9613d21e6586c88d3f088c
Nico <nicovigil1@gmail.com>
2019-04-10 20:11:51 -0600

    merge conflicts

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,4 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-<<<<<<< HEAD
 configure({ adapter: new Adapter() });
-=======
-configure({ adapter: new Adapter() });
->>>>>>> dbb6c65d6b1a0d577f6a9a3fe4b5f1e432d238ff
